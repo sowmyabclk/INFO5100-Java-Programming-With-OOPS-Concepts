01af572384c44af68438a633ecbf84fb63a34a18
sowmyabclk <55032905+sowmyabclk@users.noreply.github.com>
2019-10-14 10:59:38 -0700

    Update Contractor.java

--- Assignment-W5/Contractor.java
+++ Assignment-W5/Contractor.java
@@ -2,22 +2,23 @@ package AED.Assignment5;
 
 public class Contractor extends Employee {
 	private int workingHours;
-	int salary;
 
 	public Contractor(String name, int paymentPerHour,int workingHours) {
 		super(name, paymentPerHour);
 		this.workingHours = workingHours;
 	}
 
+
+
 	@Override
 	public int calculateSalary() {
 		
-		salary = this.workingHours * this.getPaymentPerHour();
-		return salary;
+		return this.workingHours * this.getPaymentPerHour();
 	}
 	@Override
 	public String toString() {
-		return "Contractor \n"+"Salary is: "+salary;
+		return "Contractor Salary is "+calculateSalary();
 	}
+	
 
 }
